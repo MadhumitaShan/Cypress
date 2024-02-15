72eee91c569714df244de2208f28da1a25c3fb4a
MadhumitaShan <154509455+MadhumitaShan@users.noreply.github.com>
2024-02-15 14:26:00 +0530

    Add files via upload

---
+++ cypress.config.js
@@ -0,0 +1,22 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  reporter: 'cypress-mochawesome-reporter',
+  reporterOptions: {
+    charts: true,
+    reportPageTitle: 'custom-title',
+    embeddedScreenshots: true,
+    inlineAssets: true,
+    saveAllAttempts: false,
+  },
+  video: true,
+  retries: 1,
+  e2e: {
+    setupNodeEvents(on, config) {
+      require('cypress-mochawesome-reporter/plugin')(on);
+    },
+    testIsolation: false,
+  },
+});
+
+
